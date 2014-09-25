7324a460696fdb13f52f374f135b60beddd44d67
Ryan Rentfro <rrentfro@gdrgroup.com>
2014-09-25 06:57:43 -0700

    Adds Config File and Initial Core Code

--- system/config.system.php
+++ system/config.system.php
@@ -14,4 +14,50 @@
  * @category objects
  */
 
+class asConfig
+{
+    use Singleton;
+    
+    public static $projectName = 'Asteroidea';
+    
+    public static $debugMode = true;
+    
+    public static $salt = '9825728342394582';
+    
+    public static $dbHost = 'localhost';
+    public static $dbUser = 'root';
+    public static $dbPass = '';
+    public static $dbDatabase = 'irie';
+    
+    public static $smtpHost = '';
+    public static $smtpPort = '';
+    public static $smtpFrom = '';
+    public static $smtpUser = '';
+    public static $smtpPass = '';
+    public static $smtpSSL = false;
+    
+    public static $fileStorage = '';
+    public static $logStorage = '';
+    public static $cliStorage = '';
+    public static $themeStorage = '';
+    
+    public static $htmlpurifierConfig = '';
+    
+    public static $timezone = '';
+    
+    public static $webPath = 'http://localhost/asteroidea/';
+    public static $filePath = 'c:/wamp/www/asteroidea/';
+    public static $subPath = 'asteroidea';
+    
+    public static $logUserAuth = true;
+    
+    public static $mysqlDateFormat = '%b %d %Y %h:%i %p';
+    public static $phpDateFormat = 'F j, Y, g:i a';
+    
+    protected function init()
+    {
+        
+    }
+}
+ 
 ?>
--- system/core/core.php
+++ system/core/core.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 /**
  * @package asteroidea
  * @author Ryan Rentfro, http://www.rentfro.net 
@@ -13,5 +14,21 @@
  * @var object core
  * @category objects
  */
+
+/**
+* Application Globals
+* 
+* @var core globals
+*/
+$GLOBALS['package'] = 'asteroidea';
+$GLOBALS['version'] = '0.0.1';
+$GLOBALS['app'] = '';
+$GLOBALS['filePath'] = str_replace('\system\core', '', dirname(__FILE__));
+$GLOBALS['webPath'] = $_SERVER['REQUEST_URI'];
+$GLOBALS['system'] = $GLOBALS['filePath'] . '/system/';
+
+require_once($GLOBALS['system'] . 'traits/trait.singleton.php');
+require_once($GLOBALS['system'] . 'config.system.php');
+
 
 ?>
