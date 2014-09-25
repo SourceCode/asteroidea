34035dd018c63f5aee107030060550462dfcf747
Ryan Rentfro <rrentfro@gdrgroup.com>
2014-09-25 06:04:11 -0700

    Initial Commit

---
+++ system/core/core.exceptions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package asteroidea
+ * @author Ryan Rentfro, http://www.rentfro.net 
+ * @version core.exceptions.php, v0.0.1
+ * @copyright Ryan Rentfro, http://www.rentfro.net
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @category core
+ */
+ 
+/**
+ * @access public
+ * @var object core exception handler
+ * @category objects
+ */
+
+?>
---
+++ system/core/core.model.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package asteroidea
+ * @author Ryan Rentfro, http://www.rentfro.net 
+ * @version core.model.php, v0.0.1
+ * @copyright Ryan Rentfro, http://www.rentfro.net
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @category core
+ */
+ 
+/**
+ * @access public
+ * @var object core model handler
+ * @category objects
+ */
+
+?>
